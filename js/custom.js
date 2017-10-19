b8d40430d3faca0d5f32a5c834e5f1eb12df841b
warrickct <warrickctjunk@gmail.com>
2017-10-19 13:53:31 +1300

    Aligned the weather data to be centre screen, left text aligned. Moved the weather-icon div to be beneath the date div. Added degrees to the weather values. Removed some unnecessary javascript.

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,30 @@ $(function() {
   
   window.min = 0;
   window.max = 30;
-  
+
+  //Generating the daily temperatures array and month average value
+  dayAvgTempList = [];
+  monthAvgTemp = 0;
+
+  $.get("data/july-2016-auckland-weather-data.csv", function (data) {
+    data = $.csv.toObjects(data);
+    for (day in data) {
+      daySum = 0;
+      for (var prop in data[day]) {
+        if (data[day].hasOwnProperty(prop) && prop != "day") {
+          daySum += parseInt(data[day][prop]);
+        }
+      }
+      daySum = daySum/8;
+      monthAvgTemp += daySum;
+      daySum = Math.round(daySum * 10) / 10;
+      dayAvgTempList.push(daySum);
+    }
+    monthAvgTemp = Math.round((monthAvgTemp / data.length) * 10) / 10;
+    console.log("month average temp: " + monthAvgTemp);
+    console.log(dayAvgTempList);
+  });
+
   function createSlider() {
     noUiSlider.create(window.slider, {
       start: [ window.range.min, window.range.min+1 ],
@@ -38,40 +61,6 @@ $(function() {
         }
       }
     });
-
-    //Test 1 start
-    //Declaring the variables outside anonymous function for use by update function
-    dayAvgTempList = [];
-    monthAvgTemp = 0;
-
-    $.get("data/july-2016-auckland-weather-data.csv", function (data) {
-      data = $.csv.toObjects(data);
-
-      
-
-      //loop through the data
-      for (day in data) {
-        daySum = 0;
-        //loop through the fields 1-9
-        for (var prop in data[day]) {
-          //making sure property is one of the hi's or lo's
-          if (data[day].hasOwnProperty(prop) && prop != "day") {
-            daySum += parseInt(data[day][prop]);
-          }
-        }
-        //average the daySum
-        daySum = daySum/8;
-        monthAvgTemp += daySum;
-        daySum = Math.round(daySum * 10) / 10;
-        //push the daySum to a new array
-        dayAvgTempList.push(daySum);
-      }
-      monthAvgTemp = Math.round((monthAvgTemp / data.length) * 10) / 10;
-      console.log("month average temp: " + monthAvgTemp);
-      console.log(dayAvgTempList);
-           
-    });  
-    //Test 1 end
     
     window.slider.noUiSlider.on('update', function() {
       var values = this.get();
@@ -80,51 +69,37 @@ $(function() {
       renderData();
 
       var sliderValuesInner = window.max + ", July, 2016";
+      var iconName = "";
+      var isAboveAvg = false;
+      var rangeAvg = 0;
 
-      //test 2 generating html based off slider temperatures and month temperature
-
-      //icon string holder
-      iconName = "";
-      isAboveAvg = false;
-
-      //if sliders same
       if (window.min == window.max){
-        console.log("sliders same value");
-        //if array value at index slider > month average
-        if (dayAvgTempList[window.max] >= monthAvgTemp) {
-          //set aboveavg to true
+        rangeAvg = dayAvgTempList[window.min-1];
+        rangeAvg = Math.round(rangeAvg * 10) / 10;
+        if (rangeAvg >= monthAvgTemp) {
           isAboveAvg = true;
         }
-        //else
         else {
-          //set above avg to false
           isAboveAvg = false;
         }
       }
-      //if sliders different
       else {
-        console.log("sliders different")
-        //declare sum holder
         rangeSum = 0;
         //loop from the min slider to max slider (inclusive). Offset -1
         for (i =  window.min -1; i <= window.max-1; i++) {
-          //cumulative add to the rangeSum
           rangeSum += dayAvgTempList[i];
         }
         //divide Sum by the window.max - (window.min -1) to include the window.min slider
         rangeAvg = rangeSum / (window.max - (window.min -1));
         rangeAvg = Math.round(rangeAvg * 10) / 10;
-        console.log("range average: " + rangeAvg);
-        console.log("MONTH average: " + monthAvgTemp);
-        //if range avg > monthly then icon = sun or aboveAvg = true;
         if (rangeAvg > monthAvgTemp) {
           isAboveAvg = true;
         }
         else {
           isAboveAvg = false;
         }
+        sliderValuesInner = window.min + " - " + sliderValuesInner;
       }
-
       if (isAboveAvg) {
         iconName = "sun.svg"
       }
@@ -132,21 +107,15 @@ $(function() {
         iconName = "cloud-drizzle.svg"
       }
 
-      //make the innerHTML string for icon
-      weatherIconInner = "<img src=\"assets/icons/"+ iconName +"\"/>";
-      //populate the HTML tag with the HTML string
+      //Populating the weather tag
+      weatherIconInner = "Weather: "+ rangeAvg + " &deg;C <img src=\"assets/icons/"+ iconName +"\"/>";
       $('.weather-icon').html(weatherIconInner);
-
       //test 2 end...
-
-      if (window.min != window.max){
-        sliderValuesInner = window.min + " - " + sliderValuesInner;
-      }
       var sliderValuesHtml = "<div id=\"sliderValues\">" + sliderValuesInner + "</div>";
       $('#sliderValues').html(sliderValuesHtml);
     });
-    
   }
+
   $('.play').click(function() {
     if (!window.t) {
       window.t = setInterval(function () {
